e4ac6f80497925f5e2eb5dcd7cee65d149f3ba72
OlenaKhamilivska <stadni4yk.elena@gmail.com>
2023-05-11 10:50:46 +0100

    my second project

---
+++ app/src/main/java/com/example/test2023/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.test2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onMain() {
+        Log.d("myTAG203", "onMain: 123");
+    }
+}
